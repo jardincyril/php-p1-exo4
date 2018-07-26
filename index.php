21fe61da1cfe6322cdb37075d3b177f2b80d8a95
cyril jardin <cyilroc60@hotmail.fr>
2018-07-26 15:25:10 +0200

    variable

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="utf-8"/>
+  <title>exo4</title>
+</head>
+<body>
+  <p>
+    <?php
+    $string= 'j aime regardé des séries';
+    echo $string;
+    $kg= 70;
+    echo "Je pèse $kg kg.";
+    $price=25.10;
+    echo "le prix est de $price €.";
+    $je_suis_cool= true;
+    echo $je_suis_cool;
+    ?>
+  </p>
+</body>
+</html>
